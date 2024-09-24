3b036092e2842de778b0b4cb0ca6c897f4811145
josesiyo-robbio <jose.robles.bo@gmail.com>
2024-09-23 23:31:05 -0600

    cambios

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
+import 'package:celes_store_magic/theme/theme.dart'; 
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget 
+{
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context) 
+  {
+    final materialTheme = MaterialTheme(ThemeData().textTheme);
+    return MaterialApp
+    (
       title: 'App Tiendita',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme       :   materialTheme.light(),  
+      darkTheme   :   materialTheme.dark(), 
       home: HomeScreen(),
     );
   }
